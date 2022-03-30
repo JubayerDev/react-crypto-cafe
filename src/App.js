6aa803fc7c197cb7ef267bb53c0ccb910083d1c9
JubayerDev <ajashes858@gmail.com>
2022-03-30 15:54:53 +0600

    navbar comp added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Coins from './components/Coins/Coins';
+import Contact from './components/Contact/Contact';
 import HomePage from './components/HomePage/HomePage';
 import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/contact" element={ <Contact/>}/>
       </Routes>
     </div>
   );
