a6246c45497208b526f391b8e13a11b957dc4e49
JubayerDev <ajashes858@gmail.com>
2022-03-30 21:03:14 +0600

    coins page added

---
+++ src/components/CoinDetail/CoinDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinDetail = () => {
+    return (
+        <div>
+            Coin Details
+        </div>
+    );
+};
+
+export default CoinDetail;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import CustomLink from '../CustomLink/CustomLink';
 
 const Navbar = () => {
     return (
-        <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between'>
+        <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between shadow-sm'>
             <div className='hidden md:block'><Link to="/" className='font-sans font-bold text-xl text-gray-600'>Crypto Cafe</Link></div>
             <div className='flex gap-4 text-xl'>
                 <CustomLink to="/">Home</CustomLink>
