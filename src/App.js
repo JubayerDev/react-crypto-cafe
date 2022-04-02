22d852e91817d26cc016a7e86178a0689267dae3
JubayerDev <ajashes858@gmail.com>
2022-04-02 18:56:38 +0600

    about page added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import HomePage from './components/HomePage/HomePage';
 import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About'
 
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/coins" element={<Coins />} />
-        <Route path="/contact" element={ <Contact/>}/>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={ <About/>}/>
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
