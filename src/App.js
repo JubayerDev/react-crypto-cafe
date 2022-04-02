4cd3ab45351c791672203c158fa49fa8c8e5a0ab
JubayerDev <ajashes858@gmail.com>
2022-04-02 18:49:25 +0600

    footer page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Coins from './components/Coins/Coins';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 import HomePage from './components/HomePage/HomePage';
 import Navbar from './components/Navbar/Navbar';
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/coins" element={<Coins />} />
         <Route path="/contact" element={ <Contact/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
